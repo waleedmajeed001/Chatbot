3d9cc0c267ceb0f41a8f16b182e87acabaa86cbb
waleedmajeed001 <waleedmajeed630@gmail.com>
2025-03-24 13:15:22 +0500

    t

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,1 +1,15 @@
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" /> 
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+} 
